220c13453e71280ad6946937ba2cfa14d294b0b8
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2021-03-13 23:24:37 +0530

    FUN

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,16 @@
-console.log("STARTING")
+const postman_request = require('postman-request')
 
-setTimeout(()=>{
-    console.log("@ SEM ")
-},2000) 
+const url  ='http://api.weatherstack.com/current?access_key=81ed36a8e0dd5214b9f79e5089be27dc&query=37.8267,-122.4233&units=m';
 
-setTimeout(()=>{
-    console.log("ASDF")
-},400)
+// json:true this statement help us to parse the string data to the json object and after which i can 
+//  directly use the follwing statemne console.log(response.body)
+
+postman_request({ url:url ,json : true},(error,response)=>{
+    
+    // console.log(response.body.current.pressure)
+    console.log(`It  is currently ${response.body.current.temperature} degrees out and there is ${response.body.current.feelslike} chance of raining`)
+
+    console.log(response.body.current.weather_descriptions[0])
 
 
-
-console.log("stopping")
+})
