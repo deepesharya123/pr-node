30944d2304aefa8621b0d60cd5c234aec8f79d6b
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2021-05-14 17:48:42 +0530

    CHAT_APP

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,9 +12,29 @@ const path = require('path');
 
 const publicDir = path.join(__dirname , "../public");
 const viewsPath =  path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,"../templates/partials");
 
 app.set('views',viewsPath);
 app.set('view engine','hbs');
 app.use(express.static(publicDir));
+
+let count = 0;
+
+io.on('connection',(socket)=>{
+    console.log("New WebSocket Connection");
+
+    socket.emit('countUpdated',count);
+
+    socket.on('increment',()=>{
+        count++;    
+        // socket.emit('countUpdated',count);      //socket connect to  ony one clinet
+        io.emit('countUpdated',count)    
+    })
+
+
+})
+
+
+
 
 server.listen(port,()=> console.log(`Listening on port ${port}`));
